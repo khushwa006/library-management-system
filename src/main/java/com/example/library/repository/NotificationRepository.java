e02b5c44bbe643af69dfc39e61a9d295a8961b04
khushwa006 <khushwant2204@gmail.com>
2026-03-04 18:35:39 +0530

    Create NotificationRepository.java JPA repository

---
+++ src/main/java/com/example/library/repository/NotificationRepository.java
@@ -0,0 +1,8 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByMemberId(Long memberId);
+    List<Notification> findByMemberIdAndIsRead(Long memberId, boolean isRead);
+}
