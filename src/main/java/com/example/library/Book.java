6cd83fed26dd589a1904468aa1828faa1826496f
khushwa006 <khushwant2204@gmail.com>
2026-03-04 18:29:37 +0530

    Create Book entity class for book catalog management

---
+++ src/main/java/com/example/library/Book.java
@@ -0,0 +1,75 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 50)
+    private String author;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private int publishedYear;
+
+    // Constructors, getters and setters
+
+    public Book() {}
+
+    public Book(String title, String author, String isbn, int publishedYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedYear = publishedYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(int publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+}
